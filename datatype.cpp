53ef2fda4667f4db738c1a733032ebb0287c6008
Bimmi kumari <115577045+BimmiKumari@users.noreply.github.com>
2022-11-01 14:39:19 +0530

    basic data type in c++

---
+++ datatype.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    
+    int a;
+    cin>>a;
+    cout<<a;
+    cout<<endl;
+    long l;
+    cin>>l;
+    cout<<l;
+    cout<<endl;
+    char ch;
+    cin>>ch;
+    cout<<ch;
+    cout<<endl;
+    float f;
+    cin>>f;
+    cout<<f;
+    cout<<endl;
+    double d;
+    cin>>d;
+    cout<<d;
+    cout<<endl;
+    return 0;
+}
+
