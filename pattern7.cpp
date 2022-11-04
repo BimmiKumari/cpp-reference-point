56a0e43a82b8da28129fe031048deb8acd349f1d
Bimmi kumari <115577045+BimmiKumari@users.noreply.github.com>
2022-11-04 23:12:33 +0530

    new pattern

---
+++ pattern7.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+/*
+****
+ ***
+  **
+   *
+*/
+int main()
+{
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        //first triangle
+        int space=i-1;
+        while(space)
+        {
+            cout<<" ";
+            space=space-1;
+        }
+        
+        //second trianle
+        int j=1;
+        while(j<=n-i+1)
+        {
+            cout<<"*";
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+}
