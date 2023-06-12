9af09403469c22f1666283211dc4b8a798f9ba6b
Bimmi kumari <115577045+BimmiKumari@users.noreply.github.com>
2023-06-12 22:35:37 +0530

    Leetcode problem no 104: maximum depth of binary tree

---
+++ maxdepthofbt.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+     if(root==NULL)
+     {
+         return 0;
+     }
+        if(root->left==NULL&&root->right==NULL)
+        {
+            return 1;
+        }
+        if(root->left!=NULL&&root->right==NULL)
+        {
+            return maxDepth(root->left)+1;
+        }
+        if(root->left==NULL&&root->right!=NULL)
+        {
+            return maxDepth(root->right)+1;
+        }
+        return max(maxDepth(root->left),maxDepth(root->right))+1;
+    }
+};
