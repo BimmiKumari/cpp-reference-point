b540a418589c9a2819de4e7134237d0fdcd5c172
Bimmi kumari <115577045+BimmiKumari@users.noreply.github.com>
2024-06-11 17:35:10 +0530

    Time: 11 ms (90.65%), Space: 21.4 MB (95.36%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,13 +3,14 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int i=0;
-       for(int j=i;j<nums.size();j++)
-       {
-           if(nums[j]!=0)
-           {
-               swap(nums[j],nums[i]);
-               i++;
-           }
-       } 
+        for(int j=0;j<nums.size();j++)
+        {
+            
+            if(nums[j]!=0)
+            {
+                swap(nums[i],nums[j]);
+                i++;
+            }
+        }
     }
 };
